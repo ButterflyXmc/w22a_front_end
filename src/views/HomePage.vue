4978e446fed95641c286becd27f14eb186568dd3
mahic <mahic@domain.com>
2023-02-14 12:44:20 -0700

    Had restart my front end porject because I was havinf issue with the first one

---
+++ src/views/HomePage.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <h1>Home</h1>
+        <div v-for="candy in candies" :key="candy.name">
+            <p>Id : {{candy.Id}}</p>
+            <p>Name : {{candy.Name}}</p>
+            <p>Description: {{candy.Description}}</p>
+        </div>
+
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+    export default {
+        name: "HomePage",
+        data() {
+            return {
+                candies: [],
+                Id: null,
+                name: null,
+                description: null,
+            }
+        },
+        methods: {
+            get_candy(){
+                axios.request({
+                    url: "http://127.0.0.1:5000/api/candy",
+                    method: "GET"
+                }).then((response)=>{
+                    console.log(response.data)
+                    this.candies = response.data
+                }).catch((error)=>{
+                    console.log(error)
+                })
+            }
+        },
+        mounted(){
+            this.get_candy();
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
